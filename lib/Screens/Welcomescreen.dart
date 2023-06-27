d8840898f4f31558378f4c7fa5de78d2db186b50
Siddhant <076bct083@ioepc.edu.np>
2023-06-27 21:39:08 +0545

    done validating

--- lib/Screens/Welcomescreen.dart
+++ lib/Screens/Welcomescreen.dart
@@ -15,6 +15,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Text('Welcome Screen'),),
       body: SafeArea(
         child: Container(
           child: Center(
